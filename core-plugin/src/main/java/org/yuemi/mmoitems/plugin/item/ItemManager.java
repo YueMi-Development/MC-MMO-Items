26c2deb09681dc092e42075b3dc3ed3637298a08
Muhammad Tamir <muhammad.tamir@gmail.com>
2026-07-06 20:05:02 +0700

    fix(item): ignore unrecognized json properties in config deserialization and remove unused deps

--- core-plugin/src/main/java/org/yuemi/mmoitems/plugin/item/ItemManager.java
+++ core-plugin/src/main/java/org/yuemi/mmoitems/plugin/item/ItemManager.java
@@ -35,6 +35,7 @@ public final class ItemManager {
                 .enable(JsonReadFeature.ALLOW_SINGLE_QUOTES)
                 .enable(JsonReadFeature.ALLOW_UNQUOTED_FIELD_NAMES)
                 .enable(JsonReadFeature.ALLOW_TRAILING_COMMA)
+                .disable(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .build();
     }
 
